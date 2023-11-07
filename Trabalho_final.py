4f2308fc765c6fe4f2b848192b0627db8a1c42a7
ruanrb32 <ruan.rebelo@gmail.com>
2023-11-06 23:25:38 -0400

    Add files via upload

---
+++ Trabalho_final.py
@@ -0,0 +1,15 @@
+ 
+class Agente_Aspirador_Po:
+
+    def __init__ (self):
+
+        self.energia_aspirador = 100
+        self.capacidade_bolsa = 10
+        self.localizacao = 'A'
+        self.sujeira = { 
+
+            'A' : True, 'B': False, 'C': True, 'D':False, 'E':False, 'F':True, 'G': True, 'H': False, 'I':True, 'J': False, 'K': True, 'L':False, 'M':True, 'N':False, 'O':True, 'P':False
+
+}
+
+
